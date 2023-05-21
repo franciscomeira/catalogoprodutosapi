fb2b46337ede65ab5d66541e6f1213e658ebc805
Francisco Meira <fmeirajunior@gmail.com>
2023-05-21 07:51:34 -0300

    Update files

--- script5.js
+++
@@ -1,6 +0,0 @@
-// seleciona o elemento h1 pelo seu ID
-var titulo = document.getElementById("titulo");
-const texto = "Este aqui ";
-// adiciona um novo parágrafo após o título usando innerHTML
-titulo.innerHTML += texto;
-document.write(texto);
